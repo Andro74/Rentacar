f5fa70254b2200a610358ddc3d6acdf7feace780
Vanja Mustač <vanja.mustac@skola.ss-aljari.hr>
2025-05-14 08:38:03 +0200

    Promjena komentara

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
         // Inicijalizacija mape
         const map = L.map('map').setView([45.1, 15.2], 7);
         
-        // Dodavanje OpenStreetMap sloja
+        // Dodavanje
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
